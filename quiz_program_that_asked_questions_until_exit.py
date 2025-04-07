cce2a1e26087b5e8bbc6c72fb861fc71e24ece1c
RynJyy <ryancanlas299@gmail.com>
2025-04-08 01:50:46 +0800

    add_header_and_short_manual_of_the_program

--- quiz_program_that_asked_questions_until_exit.py
+++ quiz_program_that_asked_questions_until_exit.py
@@ -3,7 +3,14 @@ import json
 import random
 from colorama import Fore, Style, init
 # Initialize colorama for colored text
+init (autoreset=True)
 # Function to display a fancy header
+print (Fore.BLUE + "=" * 34)
+print (Fore.MAGENTA + "📚 Welcome to the quiz creator! 📚")
+print (Fore.BLUE + "=" * 34 + "\n")
+# Add a description on how to use the program
+print (Fore.CYAN + "This program allows you to create your own quiz questions.")
+print (Fore.CYAN + "You can add questions, specify answers, and save them for later use.\n")
 # Function to get a quiz question from the user
 # Collect four possible answers (a, b, c, d)
 # Get the correct answer from the user (validate input)
