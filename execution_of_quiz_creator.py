7641c6cc151cf99ebeb882194448bcaa93a69a1c
RynJyy <ryancanlas299@gmail.com>
2025-04-26 19:04:30 +0800

    import_modules

--- execution_of_quiz_creator.py
+++ execution_of_quiz_creator.py
@@ -1,4 +1,8 @@
 # Import necessary modules
+import random
+import json
+import tkinter as tk
+from tkinter import messagebox
 # Load quiz data from the JSON file
 # Check if user's answer is correct
 # Display next question
