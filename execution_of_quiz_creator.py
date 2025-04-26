3fb9731f8a6b9b4c83f7aaa6164b1a8fe8aaf0be
RynJyy <ryancanlas299@gmail.com>
2025-04-26 18:21:41 +0800

    WIP: before pulling

--- execution_of_quiz_creator.py
+++ execution_of_quiz_creator.py
@@ -1,5 +1,9 @@
-
-
-
-
+# Import necessary modules
+# Load quiz data from the JSON file
+# Check if user's answer is correct
+# Display next question
+# Show final score
+# Set up main window
+# Widgets
+# Start the first question
 
